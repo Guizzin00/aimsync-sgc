043e59307d1cb50c9c10343bfe1ee69aa8fcfacc
guizzin00 <guilherme67396@estudante.ifb.edu.br>
2026-06-12 12:54:22 -0300

    Corrigindo caminho do python para o vercel achar o django

--- backend/aimsync/wsgi.py
+++ backend/aimsync/wsgi.py
@@ -1,5 +1,11 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Adiciona a pasta 'backend' ao path do Python para o Vercel conseguir achar o projeto
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aimsync.settings')
 application = get_wsgi_application()
